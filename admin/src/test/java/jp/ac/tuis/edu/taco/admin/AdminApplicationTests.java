06df4db88c9b468e650f7503abf0b94a9d3433e4
Wan Cheng <j18467tw@edu.tuis.ac.jp>
2021-04-22 16:50:40 +0900

    .

---
+++ admin/src/test/java/jp/ac/tuis/edu/taco/admin/AdminApplicationTests.java
@@ -0,0 +1,13 @@
+package jp.ac.tuis.edu.taco.admin;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AdminApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
