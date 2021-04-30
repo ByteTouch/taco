e3935ee532c182f9719361977c153b1b740be6ba
Wan Cheng <j18467tw@edu.tuis.ac.jp>
2021-04-30 19:01:44 +0900

    .

--- service/src/main/java/jp/ac/tuis/edu/taco/cloud/service/ServiceController.java
+++ service/src/main/java/jp/ac/tuis/edu/taco/cloud/service/ServiceController.java
@@ -3,11 +3,10 @@ package jp.ac.tuis.edu.taco.cloud.service;
 import jp.ac.tuis.edu.taco.cloud.service.webclient.IngredientServiceClient;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.hateoas.EntityModel;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
-
-import reactor.core.publisher.Mono;
 
 @RestController
 public class ServiceController {  
@@ -20,7 +19,7 @@ public class ServiceController {
     }
 
     @GetMapping("/ingredients/{id}")
-    public Mono<Ingredient> getIngredientById(@PathVariable("id") String id) {
-        return ingredientClient.getIngredientById(id);
+    public EntityModel<Ingredient> getIngredientById(@PathVariable("id") String id) {
+        return ingredientClient.getIngredientById(id).block();
     }
 }
